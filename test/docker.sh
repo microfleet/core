70ba43e1e175370f18bee2259f9ffc958571a3c7
AVVS <v@aminev.me>
2015-12-13 16:59:27 +0300

    chore: add multiple node test versions

--- test/docker.sh
+++ test/docker.sh
@@ -4,6 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DC="$DIR/docker-compose.yml"
 PATH=$PATH:$DIR/.bin/
 
+if ! [ -z "$NODE_VER" ]; then
+  NODE_VER="5"
+fi
+
 if ! [ -x "$(docker-compose -v)" ]; then
   mkdir $DIR/.bin
   curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > $DIR/.bin/docker-compose
@@ -14,7 +18,7 @@ docker-compose -f $DC up -d
 docker run --link rabbitmq --link redis_client \
   --link redis_2 --link redis_3 --link redis_1 \
   --rm -it --name 'mservice-test' -e 'BLUEBIRD_DEBUG=1' \
-  -v "$PWD":/usr/src/app -w /usr/src/app node:5 \
+  -v "$PWD":/usr/src/app -w /usr/src/app node:$NODE_VER \
   ./node_modules/.bin/mocha --require ./test/babelhook.js --bail -R spec
 exitCode=$?
 docker-compose -f $DC stop
