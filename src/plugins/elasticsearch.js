4efcba5e663c6f721503ffae3fe00839e80a19b2
Evgeny Poyarkov <eug.poyarkov@gmail.com>
2016-05-01 15:02:18 +0300

    Feature/elastic plugin (#59)
    feat(plugin): adds elasticsearch support
    
    * feat: adds elasticsearch plugin
    * chore: bump version of babel-eslint
    * chore: add elasticsearch to devDependencies

---
+++ src/plugins/elasticsearch.js
@@ -0,0 +1,61 @@
+const Errors = require('common-errors')
+const Promise = require('bluebird')
+const Elasticsearch = require('elasticsearch')
+
+exports.name = 'elasticsearch'
+
+exports.attach = function attachElasticsearch(conf = {}) {
+  const service = this;
+
+  // optional validation with the plugin
+  if (typeof service.validateSync === 'function') {
+    const isConfValid = service.validateSync('elasticsearch', conf);
+    if (isConfValid.error) {
+      throw isConfValid.error;
+    }
+  }
+
+  return {
+    /**
+     * @private
+     * @return {Promise}
+     */
+    connect: function connectElasticsearch() {
+      if (service._elasticsearch) {
+        return Promise.reject(new Errors.NotPermittedError('elasticsearch was already started'));
+      }
+
+      const instance = new Elasticsearch.Client({ ...conf,
+        defer: function() {
+          const defer = {};
+          
+          defer.promise = new Promise((resolve, reject) => {
+            defer.resolve = resolve;
+            defer.reject = reject;
+          });
+
+          return defer;
+        }
+      });
+
+      return instance.nodes.info({ human: true }).then(() => {
+        service._elasticsearch = instance;
+        service.emit('plugin:connect:elasticsearch', instance);
+        return instance;
+      });
+    },
+
+    /**
+     * @private
+     * @return {Promise}
+     */
+    close: function disconnectElasticsearch() {
+      return Promise.try(() => service._elasticsearch.close())
+        .then(function cleanupElasticsearchRef() {
+          service._elasticsearch = null;
+          service.emit('plugin:close:elasticsearch');
+          return true;
+        });
+    }
+  };
+};
