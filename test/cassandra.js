9c037c9b25522338b2db052dd0ddd01c577bd003
Aleksandr Sorokin <imarkoffko@gmail.com>
2016-06-10 13:15:22 +0300

    plugins: add cassandra plugin

---
+++ test/cassandra.js
@@ -0,0 +1,57 @@
+const Cassandra = require('express-cassandra');
+const cloneDeep = require('lodash/cloneDeep');
+const { expect } = require('chai');
+
+describe('Cassandra suite', function testSuite() {
+  const Mservice = require('../src');
+
+  it('able to connect to cassandra when plugin is included', function test() {
+    this.service = new Mservice({
+      plugins: ['validator', 'cassandra'],
+      cassandra: global.SERVICES.cassandra
+    });
+
+    return this.service.connect()
+      .reflect()
+      .then(result => {
+        expect(result.isFulfilled()).to.be.eq(true);
+        return Promise.resolve(result.value());
+      })
+      .spread(cassandra => {
+        expect(cassandra).to.be.instanceof(Cassandra);
+        expect(cassandra.modelInstance).to.have.property('Foo');
+        expect(this.service.cassandra).to.be.instanceof(Cassandra);
+      });
+  });
+
+  it('able to close connection to cassandra', function test() {
+    return this.service.close()
+      .reflect()
+      .then(result => {
+        expect(result.isFulfilled()).to.be.eq(true);
+        expect(() => {
+          return this.service.cassandra;
+        }).to.throw();
+      });
+  });
+
+  it('should load models from directory', function test() {
+    const cassandraConfig = cloneDeep(global.SERVICES.cassandra);
+    cassandraConfig.service.models = __dirname + '/cassandra/models';
+
+    this.service = new Mservice({
+      plugins: ['validator', 'cassandra'],
+      cassandra: cassandraConfig
+    });
+
+    return this.service.connect()
+      .reflect()
+      .then(result => {
+        expect(result.isFulfilled()).to.be.eq(true);
+        return Promise.resolve(result.value());
+      })
+      .spread(cassandra => {
+        expect(this.service.cassandra.modelInstance).to.have.property('Bar');
+      });
+  });
+});
---
+++ test/cassandra/models/BarModel.js
@@ -0,0 +1,6 @@
+module.exports = {
+  fields:{
+    baz: 'text'
+  },
+  key:['baz']
+};
