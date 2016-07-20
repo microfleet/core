8bdbf985417ee198934903c2bd81e2b9d8d3ad1f
Aleksandr Sorokin <imarkoffko@gmail.com>
2016-07-21 02:42:54 +0300

    fix(socketIO): fix filenames case

---
+++ src/plugins/socketIO.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const debug = require('debug')('mservice:socketIO');
+const is = require('is');
+const SocketIO = require('socket.io');
+const attachRouter = require('./socketIO/router/attach');
+
+/**
+ * @todo add adapter factory
+ */
+function attachSocketIO(config = {}) {
+  debug('Attaching socketIO plugin');
+  const service = this;
+
+  if (is.fn(this.validateSync)) {
+    assert.ifError(this.validateSync('socketIO', config).error);
+  }
+
+  const socketIO = new SocketIO(config.options);
+
+  if (config.router.enabled) {
+    attachRouter(socketIO, config.router, service.router);
+  }
+
+  this._socketIO = socketIO;
+}
+
+module.exports = {
+  attach: attachSocketIO,
+  name: 'socketIO',
+};
