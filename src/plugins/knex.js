fa922b8e71770d4a5f63f6d9ab6d3aec9e9b8c22
Aleksandr Sorokin <imarkoffko@gmail.com>
2016-10-07 17:59:27 +0300

    feat: knex plugin

---
+++ src/plugins/knex.js
@@ -0,0 +1,47 @@
+const _require = require('../utils/require');
+const assert = require('assert');
+const Errors = require('common-errors');
+
+const factory = _require('knex');
+
+function attachKnex(config = {}) {
+  const service = this;
+
+  if (this._log === undefined) {
+    throw new Errors.NotFoundError('log module must be included');
+  }
+
+  if (this._validator === undefined) {
+    throw new Errors.NotFoundError('validator module must be included');
+  }
+
+  assert.ifError(this.validator.validateSync('knex', config).error);
+  assert.ifError(this.validator.validateSync(`knex.${config.client}`, config).error);
+
+  const knex = this._knex = factory(config);
+
+  return {
+    connect: function connectKnex() {
+      return knex
+        .raw('SELECT TRUE;')
+        .then((result) => {
+          assert.equal(result.rows[0].bool, true);
+          service.emit('plugin:connect:knex', knex);
+
+          return knex;
+        });
+    },
+
+    close: function disconnectKnex() {
+      return knex
+        .destroy()
+        .then(() => service.emit('plugin:close:knex'));
+    },
+  };
+}
+
+module.exports = {
+  attach: attachKnex,
+  name: 'knex',
+  type: 'database',
+};
