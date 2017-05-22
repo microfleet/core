ce0d4f6effecadea1d474f922e21475a06caa60f
Aleksandr Sorokin <imarkoffko@gmail.com>
2017-05-22 22:27:42 +0300

    fix: tests

--- test/fixtures/migrations/script-migration/index.js
+++ test/fixtures/migrations/script-migration/index.js
@@ -5,11 +5,10 @@ const script = fs.readFileSync(`${__dirname}/migration.lua`, 'utf8');
 exports.min = 2;
 exports.final = 10;
 
-exports.script = (service, pipeline, versionKey, appendLuaScript) => service.redis
-  .get('cardinality')
-  .return(10)
-  .then((cardinality) => {
-    const lua = appendLuaScript(exports.final, exports.min, script);
-    debug('evaluating', lua);
-    pipeline.eval(lua, 2, versionKey, 'some-index-key', cardinality);
-  });
+exports.script = ({ redis }) =>
+  redis
+    .get('cardinality')
+    .return(10)
+    .then((cardinality) => {
+      redis.eval(script, 1, 'some-index-key', cardinality);
+    });
