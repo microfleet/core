e5ca2677c295207825d9aba5a037cd0854f5d177
Eugene Poyarkov <eug.poyarkov@gmail.com>
2018-10-17 16:44:17 +0300

    feat: throw an error in unhealthy conditions

--- src/plugins/router/routes/generic/health.js
+++ src/plugins/router/routes/generic/health.js
@@ -1,7 +1,13 @@
-const { ActionTransport } = require('../../../../constants');
+const { HttpStatusError } = require('common-errors');
+const { ActionTransport, PLUGIN_STATUS_FAIL } = require('../../../../constants');
 
 async function GenericHealthCheck(): Object {
   const data = await this.getHealthStatus();
+
+  if (PLUGIN_STATUS_FAIL === data.status) {
+    throw HttpStatusError(500, `Unhealthy due to following plugins: ${data.failed.map(it => it.name).join(', ')}`);
+  }
+
   return { data };
 }
 
