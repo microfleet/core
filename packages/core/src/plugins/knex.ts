fc3af6ae52e6aae92231b9d3f8b5ab734607bdcd
AVVS <v@makeomatic.ru>
2018-10-19 01:01:07 -0700

    fix: ensure knex retries the connection several times

--- packages/core/src/plugins/knex.ts
+++ packages/core/src/plugins/knex.ts
@@ -2,6 +2,7 @@ import assert = require('assert')
 import { NotFoundError } from 'common-errors'
 import { Microfleet, PluginTypes } from '../'
 import _require from '../utils/require'
+import retry = require('bluebird-retry')
 
 export const name = 'knex'
 export const type = PluginTypes.database
@@ -17,11 +18,21 @@ export function attach(this: Microfleet, config: any = {}) {
 
   const knex = service.knex = factory(config)
 
+  const establishConnection = async () => {
+    const result = await knex.raw('SELECT TRUE;')
+    assert.equal(result.rows[0].bool, true)
+  }
+
   return {
     async connect() {
-      const result = await knex.raw('SELECT TRUE;')
+      await retry(establishConnection, {
+        interval: 500,
+        backoff: 2,
+        max_interval: 5000,
+        timeout: 60000,
+        max_tries: 100,
+      })
 
-      assert.equal(result.rows[0].bool, true)
       service.addMigrator('knex', () => knex.migrate.latest())
       service.emit('plugin:connect:knex', knex)
 
