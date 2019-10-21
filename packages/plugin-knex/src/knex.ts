f2b62409bdadea7627eb5e71aa1a53cf1fd85409
AVVS <v@makeomatic.ca>
2019-10-21 12:46:25 -0700

    chore: adjust plugin interface

--- packages/plugin-knex/src/knex.ts
+++ packages/plugin-knex/src/knex.ts
@@ -1,6 +1,6 @@
 import assert = require('assert')
 import { NotFoundError } from 'common-errors'
-import { Microfleet, PluginTypes, LoggerPlugin } from '@microfleet/core'
+import { Microfleet, PluginTypes, LoggerPlugin, PluginInterface } from '@microfleet/core'
 import retry = require('bluebird-retry')
 import Knex = require('knex')
 
@@ -13,6 +13,41 @@ export const type = PluginTypes.database
 export interface KnexPlugin {
   knex: Knex
 }
+
+/**
+ * Defines closure
+ */
+const startupHandlers = (service: Microfleet, knex: Knex): PluginInterface => ({
+  async connect() {
+    const establishConnection = async () => {
+      try {
+        const result = await knex.raw('SELECT TRUE;')
+        assert.equal(result.rows[0].bool, true)
+      } catch (err) {
+        service.log.warn({ err }, 'Failed to connect to PGSQL')
+        throw err
+      }
+    }
+
+    await retry(establishConnection, {
+      interval: 500,
+      backoff: 2,
+      max_interval: 5000,
+      timeout: 60000,
+      max_tries: 100,
+    })
+
+    service.addMigrator('knex', () => knex.migrate.latest())
+    service.emit('plugin:connect:knex', knex)
+
+    return knex
+  },
+
+  async close() {
+    await knex.destroy()
+    service.emit('plugin:close:knex')
+  },
+})
 
 export function attach(
   this: Microfleet & LoggerPlugin,
@@ -31,35 +66,5 @@ export function attach(
 
   const knex = service.knex = Knex(config)
 
-  const establishConnection = async () => {
-    try {
-      const result = await knex.raw('SELECT TRUE;')
-      assert.equal(result.rows[0].bool, true)
-    } catch (err) {
-      this.log.warn({ err }, 'Failed to connect to PGSQL')
-      throw err
-    }
-  }
-
-  return {
-    async connect() {
-      await retry(establishConnection, {
-        interval: 500,
-        backoff: 2,
-        max_interval: 5000,
-        timeout: 60000,
-        max_tries: 100,
-      })
-
-      service.addMigrator('knex', () => knex.migrate.latest())
-      service.emit('plugin:connect:knex', knex)
-
-      return knex
-    },
-
-    async close() {
-      await knex.destroy()
-      service.emit('plugin:close:knex')
-    },
-  }
+  return startupHandlers(service, knex)
 }
