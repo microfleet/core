e1087867cfb217cf8ecc63bc372c6c05aea130d4
BelkinaDasha <belkinadasha@gmail.com>
2019-12-10 23:23:31 +0500

    test(amqp): cover routing and retries

--- packages/core/test/suites/amqp.js
+++ packages/core/test/suites/amqp.js
@@ -1,10 +1,12 @@
 const Promise = require('bluebird');
 const assert = require('assert');
 const sinon = require('sinon');
+const path = require('path');
 const AMQPTransport = require('@microfleet/transport-amqp');
+const Errors = require('common-errors');
 const { findHealthCheck } = require('../utils');
 
-describe('AMQP suite', function testSuite() {
+describe('AMQP suite: lifecycle', function testSuite() {
   require('../config');
   const { Microfleet, ActionTransport } = require('../..');
 
@@ -64,5 +66,213 @@ describe('AMQP suite', function testSuite() {
     assert(consumerSpy.called);
     assert(consumerSpy.calledAfter(waitRequestFinishSpy));
     assert(consumerSpy.calledAfter(closeSpy));
+  });
+});
+
+describe('AMQP suite: basic routing', function testSuite() {
+  require('../config');
+  const { Microfleet, ActionTransport } = require('../..');
+
+  let service;
+  before(async function () {
+    service = new Microfleet({
+      name: 'tester',
+      plugins: ['logger', 'validator', 'opentracing', 'amqp', 'router'],
+      amqp: {
+        ...global.SERVICES.amqp,
+        router: {
+          enabled: true,
+          prefix: '',
+        },
+      },
+      router: {
+        extensions: { register: [] },
+        routes: {
+          directory: path.resolve(__dirname, '../amqp/helpers/actions'),
+          transports: [
+            ActionTransport.amqp,
+          ],
+        },
+      },
+    });
+    await service.connect();
+  });
+  after(async function () {
+    await service.close();
+
+    service = null;
+  });
+
+  it('able to attach amqp plugin with router enabled', async () => {
+    const { amqp } = service;
+
+    assert.ok(amqp instanceof AMQPTransport);
+    assert.doesNotThrow(() => service.amqp);
+  });
+
+  it ('able to observe an action', async () => {
+    const { amqp: amqpRoutes } = service.router.routes;
+
+    assert.ok(typeof amqpRoutes.echo === 'function');
+  })
+
+  it ('able to dispatch action and return response', async () => {
+    const { amqp } = service;
+
+    const response = await amqp.publishAndWait('echo', { foo: 'bar' });
+
+    assert.deepStrictEqual(response, { foo: 'bar' });
+  });
+});
+
+describe('AMQP suite: prefixed routing', function testSuite() {
+  require('../config');
+  const { Microfleet, ActionTransport } = require('../..');
+
+  let service;
+  before(async function () {
+    service = new Microfleet({
+      name: 'tester',
+      plugins: ['logger', 'validator', 'opentracing', 'amqp', 'router'],
+      amqp: {
+        ...global.SERVICES.amqp,
+        router: {
+          enabled: true,
+          prefix: 'amqp-prefix',
+        },
+      },
+      router: {
+        extensions: { register: [] },
+        routes: {
+          directory: path.resolve(__dirname, '../amqp/helpers/actions'),
+          transports: [
+            ActionTransport.amqp,
+          ],
+        },
+      },
+    });
+    await service.connect();
+  });
+  after(async function () {
+    await service.close();
+
+    service = null;
+  });
+
+  it ('able to observe an action', async () => {
+    const { amqp: amqpRoutes } = service.router.routes;
+
+    assert.ok(typeof amqpRoutes.echo === 'function');
+  });
+
+  it ('able to dispatch action and return response', async () => {
+    const { amqp } = service;
+
+    const response = await amqp.publishAndWait('amqp-prefix.echo', { foo: 'bar' });
+
+    assert.deepStrictEqual(response, { foo: 'bar' });
+  });
+});
+
+describe('AMQP suite: retry', function testSuite() {
+  require('../config');
+  const { Microfleet, ActionTransport } = require('../..');
+
+  let service;
+  before(async function () {
+    const failedActionEmulator = [{
+      point: 'preHandler',
+      async handler(request) {
+        const { failAtRetryCount } = request.params;
+        const { headers } = request.headers;
+        const retryCount = headers['x-retry-count'] || 0;
+        if (retryCount <= failAtRetryCount) {
+          throw new Errors.ConnectionError('Fake connection error first three times');
+        }
+        return [request];
+      },
+    }];
+
+    service = new Microfleet({
+      name: 'tester',
+      plugins: ['logger', 'validator', 'opentracing', 'amqp', 'router'],
+      amqp: {
+        transport: {
+          ...global.SERVICES.amqp.transport,
+          queue: 'test-queue',
+          bindPersistantQueueToHeadersExchange: true,
+          neck: 10,
+        },
+        router: {
+          enabled: true,
+          prefix: 'amqp-prefix',
+        },
+        retry: {
+          enabled: true,
+          min: 100,
+          max: 30 * 60 * 1000,
+          factor: 1.2,
+          maxRetries: 3, // 3 attempts only
+          predicate(error, actionName) {
+            if (actionName === 'echo') {
+              return false;
+            }
+
+            return true;
+          },
+        },
+      },
+      router: {
+        extensions: {
+          enabled: ['preHandler'],
+          register: [failedActionEmulator]
+        },
+        routes: {
+          directory: path.resolve(__dirname, '../amqp/helpers/actions'),
+          transports: [
+            ActionTransport.amqp,
+          ],
+        },
+      },
+    });
+    await service.connect();
+  });
+  after(async function () {
+    await service.close();
+
+    service = null;
+  });
+
+  it('able to attach amqp plugin with configured retry', async () => {
+    const { amqp } = service;
+
+    assert.ok(amqp instanceof AMQPTransport);
+    assert.doesNotThrow(() => service.amqp);
+  });
+
+  it ('able to successfully retry action dispatch', async () => {
+    const { amqp } = service;
+
+    const response = await amqp
+      .publishAndWait('amqp-prefix.echo', { failAtRetryCount: 2 });
+
+    assert.deepStrictEqual(response, { failAtRetryCount: 2 });
+  });
+
+  it ('able to fail when retry count exceeds max retry attempt count', async () => {
+    const { amqp } = service;
+
+    await assert.rejects(
+      amqp.publishAndWait('amqp-prefix.echo', { failAtRetryCount: 3 }),
+      {
+        message: 'Fake connection error first three times',
+        name: 'ConnectionError',
+        generateMessage: null,
+        global_initialize: undefined,
+        inner_error: undefined,
+        isOperational: true,
+        retryAttempt: 3
+      }
+    );
   });
 });
