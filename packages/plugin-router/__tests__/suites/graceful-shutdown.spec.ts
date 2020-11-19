cb1f740a53fb4cea568d8ab6c40c20eb539be5c7
Sasha Sorokin <sorokin.sh@gmail.com>
2020-11-19 20:48:45 +0300

    fix: graceful test

--- packages/plugin-router/__tests__/suites/graceful-shutdown.spec.ts
+++ packages/plugin-router/__tests__/suites/graceful-shutdown.spec.ts
@@ -1,3 +1,4 @@
+import * as wtfnode from 'wtfnode'
 import { delay, TimeoutError } from 'bluebird'
 import { resolve } from 'path'
 import { strict as assert } from 'assert'
@@ -66,10 +67,20 @@ class ChildServiceRunner {
     this.processClosed = once(subProcess, 'close')
 
     try {
-      await Promise.race([
-        once(subProcess, 'ready'),
-        delay(100000).throw(new TimeoutError()),
-      ])
+      await (new Promise((resolve, reject) => {
+        once(subProcess, 'ready').then(() => {
+          clearTimeout(timeout)
+          resolve()
+        })
+        const timeout = setTimeout(() => {
+          clearTimeout(timeout)
+          reject(new TimeoutError())
+        }, 1000 * 59)
+      }))
+      // await Promise.race([
+      //   once(subProcess, 'ready'),
+      //   delay(1000 * 59).throw(new TimeoutError()),
+      // ])
     } catch (e: any) {
       // eslint-disable-next-line no-console
       console.error(e)
@@ -105,7 +116,8 @@ class ChildServiceRunner {
     })
 
     return {
-      amqp: service.amqp.publishAndWait.bind(service.amqp),
+      service,
+      socketioClient,
       http: getHTTPRequest({ url: `http://0.0.0.0:${this.getPort()}` }),
       socketio: getSocketioRequest(socketioClient),
     }
@@ -114,8 +126,31 @@ class ChildServiceRunner {
   async kill(signal = 'SIGTERM', wait = false) {
     assert(this.serviceStarted, 'No service started')
     assert(this.process, 'No service started')
+
     if (wait) await delay(500)
+
     process.kill(this.process.pid, signal)
+
+    return new Promise((resolve, reject) => {
+      const timeout = setTimeout(() => {
+        clearTimeout(timeout)
+        clearInterval(interval)
+        reject()
+      }, 1000 * 60 * 2)
+      const interval = setInterval(() => {
+        // eslint-disable-next-line no-console
+        assert(this.process, 'No service started')
+        // eslint-disable-next-line no-console
+        console.log(`Is ${this.process.pid} alive?`)
+        try {
+          process.kill(this.process.pid, 0)
+        } catch (error) {
+          clearTimeout(timeout)
+          clearInterval(interval)
+          resolve()
+        }
+      }, 1000 * 5)
+    })
   }
 
   getPort() {
@@ -134,7 +169,7 @@ class ChildServiceRunner {
   }
 }
 
-jest.setTimeout(100000)
+jest.setTimeout(1000 * 60 * 3)
 
 describe('service graceful shutdown', () => {
   let childService: ChildServiceRunner
@@ -161,59 +196,80 @@ describe('service graceful shutdown', () => {
   it('should wait for amqp request when shutting down', async () => {
     const serviceConnector = await childService.getServiceConnectors()
 
-    const [serviceResponse] = await Promise.all([
-      serviceConnector.amqp('amqp.action.long-running', { pause: 999 }),
-      childService.kill('SIGTERM', true),
-    ])
+    try {
+      const [serviceResponse] = await Promise.all([
+        serviceConnector.service.amqp.publishAndWait('amqp.action.long-running', { pause: 999 }),
+        childService.kill('SIGTERM', true),
+      ])
 
-    assert(serviceResponse, 'should respond to action')
-    assert(serviceResponse.success)
+      assert(serviceResponse, 'should respond to action')
+      assert(serviceResponse.success)
+    } finally {
+      await serviceConnector.service.close()
+      serviceConnector.socketioClient.close()
+    }
   })
 
   it('should wait for http request when shutting down', async () => {
     const serviceConnector = await childService.getServiceConnectors()
 
-    const [serviceResponse] = await Promise.all([
-      serviceConnector.http('/action.long-running', { pause: 1000 }),
-      childService.kill('SIGTERM', true),
-    ])
+    try {
+      const [serviceResponse] = await Promise.all([
+        serviceConnector.http('/action.long-running', { pause: 1000 }),
+        childService.kill('SIGTERM', true),
+      ])
 
-    assert(serviceResponse, 'should respond to action')
-    assert(serviceResponse.success)
+      assert(serviceResponse, 'should respond to action')
+      assert(serviceResponse.success)
+    } finally {
+      await serviceConnector.service.close()
+      serviceConnector.socketioClient.close()
+    }
   })
 
   it('should wait for socket.io request', async () => {
     const serviceConnector = await childService.getServiceConnectors()
 
-    const [serviceResponseSocket] = await Promise.all([
-      serviceConnector.socketio('action.long-running', { pause: 500 }),
-      childService.kill('SIGTERM', true),
-    ])
+    try {
+      const [serviceResponseSocket] = await Promise.all([
+        serviceConnector.socketio('action.long-running', { pause: 500 }),
+        childService.kill('SIGTERM', true),
+      ])
 
-    assert(serviceResponseSocket, 'should respond to action')
-    assert(serviceResponseSocket.success)
+      assert(serviceResponseSocket, 'should respond to action')
+      assert(serviceResponseSocket.success)
+    } finally {
+      await serviceConnector.service.close()
+      serviceConnector.socketioClient.close()
+    }
   })
 
   it('should wait for multiple requests to finish', async () => {
     const serviceConnector = await childService.getServiceConnectors()
     const actions = [
-      () => serviceConnector.amqp('amqp.action.long-running', { pause: 299 }),
-      () => serviceConnector.http('/action.long-running', { pause: 500 }),
+      () => serviceConnector.service.amqp.publishAndWait('amqp.action.long-running', { pause: 299 }),
+      () => serviceConnector.http('/action/long-running', { pause: 500 }),
       () => serviceConnector.socketio('action.long-running', { pause: 500 }),
     ]
 
-    const responses = await Promise.all([
-      ...Array.from({ length: 100 })
-        .map(() => Math.floor(Math.random() * actions.length))
-        .map((i) => actions[i]()),
-      childService.kill('SIGTERM', true),
-    ])
+    try {
+      const responses = await Promise.all([
+        ...Array.from({ length: 100 })
+          .map(() => Math.floor(Math.random() * actions.length))
+          .map((i) => actions[i]()),
+        childService.kill('SIGTERM', true),
+      ])
 
-    responses.pop()
-    responses.every((resp) => {
-      assert(resp, 'should respond to action')
-      assert(resp.success)
-      return true
-    })
+      responses.pop()
+      responses.every((resp) => {
+        assert(resp, 'should respond to action')
+        assert(resp.success)
+        return true
+      })
+    } finally {
+      await serviceConnector.service.close()
+      serviceConnector.socketioClient.close()
+      wtfnode.dump()
+    }
   })
 })
