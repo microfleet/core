f4f2de825fd69b4420f3028311f3fce79bd97a3e
Sasha Sorokin <sorokin.sh@gmail.com>
2021-03-08 22:46:56 +0400

    fix(tests): upgrade transport-amqp

--- packages/plugin-router/__tests__/artifacts/actions/maintenance/http-amqp.ts
+++ packages/plugin-router/__tests__/artifacts/actions/maintenance/http-amqp.ts
@@ -1,8 +1,7 @@
 import { Microfleet } from '@microfleet/core'
 import { ActionTransport } from '@microfleet/plugin-router'
-import { AMQPPlugin } from '@microfleet/plugin-amqp'
 
-export default async function handler(this: Microfleet & AMQPPlugin): Promise<any> {
+export default async function handler(this: Microfleet): Promise<any> {
   await this.amqp.publishAndWait('maintenance.amqp', {
     some: 'data',
   })
