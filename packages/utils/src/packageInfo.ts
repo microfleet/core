9f86c9980479d433699b629a110007a0acd49dd8
Sasha Sorokin <sorokin.sh@gmail.com>
2021-03-28 21:16:28 +0300

    fix(utils): bump version for read-pkg-up to 8.0.0

--- packages/utils/src/packageInfo.ts
+++ packages/utils/src/packageInfo.ts
@@ -1,8 +1,8 @@
-import readPkgUp = require('read-pkg-up')
+import { readPackageUpSync } from 'read-pkg-up'
 import assert = require('assert')
 
 function getVersion(): string {
-  const pkgUp = readPkgUp.sync()
+  const pkgUp = readPackageUpSync()
   const version = pkgUp && pkgUp.packageJson && pkgUp.packageJson.version || ''
   assert(version, 'unable to find package.json or .version does not exist')
   return version
