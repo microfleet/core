c7d52edc236eec1cf54110c5edb32cea7a46d864
Sasha S <sorokin.sh@gmail.com>
2021-06-09 13:13:52 +0300

    fix: read-pkg-up v7

--- packages/utils/src/packageInfo.ts
+++ packages/utils/src/packageInfo.ts
@@ -1,8 +1,8 @@
-import { readPackageUpSync } from 'read-pkg-up'
+import { sync } from 'read-pkg-up'
 import { strict as assert } from 'assert'
 
 function getVersion(): string {
-  const pkgUp = readPackageUpSync()
+  const pkgUp = sync()
   const version = pkgUp && pkgUp.packageJson && pkgUp.packageJson.version || ''
 
   assert(version, 'unable to find package.json or .version does not exist')
