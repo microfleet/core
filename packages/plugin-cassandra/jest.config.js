c6569df40ee47e6839dd59cefc60d7fcb16ec549
AVVS <v@makeomatic.ca>
2021-11-18 13:51:03 -0800

    chore: fix cassandra tests

--- packages/plugin-cassandra/jest.config.js
+++ packages/plugin-cassandra/jest.config.js
@@ -1,8 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { basename } = require('path')
 const globalConfig = require('../../jest.config')
+const dir = basename(__dirname)
 
 module.exports = {
   ...globalConfig,
   rootDir: '../../',
-  testMatch: ["<rootDir>/packages/plugin-cassandra/src/**/*.spec.ts"]
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  collectCoverage: true,
+  coverageDirectory: `packages/${dir}/coverage`,
+  coverageReporters: [ "text-summary", "json", "lcov" ],
+  collectCoverageFrom: [
+    `packages/${dir}/src/**/*`,
+  ],
 }
