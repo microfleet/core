7197fe05c2585da7475fb6855a6f4fcdd7ee4649
AVVS <v@makeomatic.ca>
2022-01-30 13:20:53 -0800

    chore(tooling): custom release-it plugin for mass release
    allows to update versions of changed packages, generate separate
    changelogs & create a generic changelog/single release for the whole
    repo

---
+++ .release-it.root.cjs
@@ -0,0 +1,37 @@
+const path = require('path')
+
+module.exports = {
+  git: {
+    requireCleanWorkingDir: false,
+    tag: true,
+    commit: true,
+    requireBranch: 'master',
+    commitMessage: 'chore(release): v${version} -- updated ${affectedModules} modules [skip ci]\n\n${updateLog}',
+    tagName: 'microfleet@v${version}',
+    tagAnnotation: 'Release ${stagedModule.version}\n\n${stagedModule.changelog}',
+    pnpm: true,
+    changelog: true,
+    infile: 'CHANGELOG.md',
+  },
+  npm: {
+    publish: false,
+  },
+  github: {
+    release: true,
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      infile: false,
+      preset: {
+        name: 'angular',
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        }
+      }
+    },
+    [path.resolve(__dirname, './ci/pnpm-git.js')]: {
+
+    }
+  },
+  'disable-metrics': true,
+}
