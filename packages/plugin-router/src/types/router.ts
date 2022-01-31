40ece1e8e701ac3e3f34f97016b1a94f517da515
AVVS <v@makeomatic.ca>
2022-01-31 13:08:52 -0800

    revert: router types exploration

--- packages/plugin-router/src/types/router.ts
+++ packages/plugin-router/src/types/router.ts
@@ -1,53 +1,14 @@
 import { ClientRequest } from 'http'
-import type { Span } from 'opentracing'
+import { Span } from 'opentracing'
 import { Microfleet } from '@microfleet/core'
-import type { Logger as MicrofleetLoggerInstance } from '@microfleet/plugin-logger'
-import type * as http from 'http'
-import type * as https from 'https'
-import type * as http2 from 'http2'
+import { Logger } from '@microfleet/plugin-logger'
 import { RequestDataKey, ActionTransport } from '../router'
 
-export type RequestBodyDefault = unknown
-export type RequestQuerystringDefault = unknown
-export type RequestParamsDefault = unknown
-export type RequestHeadersDefault = unknown
-export type ReplyDefault = unknown
-
-/**
- * A union type of the Node.js server types from the http, https, and http2 modules.
- */
-export type RawServerBase = http.Server | https.Server | http2.Http2Server | http2.Http2SecureServer
-
-/**
- * The default server type
- */
-export type RawServerDefault = http.Server
-
-/**
- * The default request type based on the server type. Utilizes generic constraining.
- */
-export type RawRequestDefaultExpression<
-  RawServer extends RawServerBase = RawServerDefault
-  > = RawServer extends http.Server | https.Server ? http.IncomingMessage
-  : RawServer extends http2.Http2Server | http2.Http2SecureServer ? http2.Http2ServerRequest
-  : never
-
-export interface RequestGenericInterface {
-  Body?: RequestBodyDefault;
-  Querystring?: RequestQuerystringDefault;
-  Params?: RequestParamsDefault;
-  Headers?: RequestHeadersDefault;
-}
-
 export type ServiceMiddleware = (this: Microfleet, request: ServiceRequest) => Promise<void>
+export type ServiceActionHandler<R = unknown> = (this: Microfleet, request: ServiceRequest, ...params: any) => Promise<R>
 export type ServiceActionAuthGetName = (request: ServiceRequest) => string
 
-export interface ServiceAction<
-  RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-> {
-  (this: Microfleet, request: ServiceRequest<RouteGeneric, RawServer, RawRequest>): Promise<Reply>
+export interface ServiceAction<R = unknown> extends ServiceActionHandler<R> {
   actionName: string
   transports: ServiceRequest['transport'][]
   transportOptions?: TransportOptions
@@ -60,35 +21,20 @@ export interface ServiceAction<
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface TransportOptions {}
 
-export interface ReplyGenericInterface {
-  Reply?: ReplyDefault;
-}
-
-export interface RouteGenericInterface extends RequestGenericInterface, ReplyGenericInterface { }
-
-/**
- * FastifyRequest is an instance of the standard http or http2 request objects.
- * It defaults to http.IncomingMessage, and it also extends the relative request object.
- */
-export interface ServiceRequest<
-  RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  > {
-  id: any;
-  params: RouteGeneric['Params'];
-  query: RouteGeneric['Querystring'];
-  headers: RawRequest['headers'] & RouteGeneric['Headers']; // this enables the developer to extend the existing http(s|2) headers list
-  body: RouteGeneric['Body'];
-  log: MicrofleetLoggerInstance;
+export interface ServiceRequest<R = unknown> {
   route: string
+  action: ServiceAction<R>
+  params: any
+  headers: any
+  query: any
   method: keyof typeof RequestDataKey
   transport: typeof ActionTransport[keyof typeof ActionTransport]
   transportRequest: any | ClientRequest
   locals: any
-  parentSpan?: Span
+  parentSpan: any
+  span?: Span
+  log: Logger
   response?: unknown
   error?: any
   reformatError: boolean
-  action: ServiceAction<R>
 }
