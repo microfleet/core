1083ffde24dc700be872fba80d5725c90c1adca4
AVVS <v@makeomatic.ca>
2022-02-27 23:32:21 +0000

    chore(tooling): npm version fixer

--- ci/conventional-changelog-microfleet.js
+++ ci/conventional-changelog-microfleet.js
@@ -16,7 +16,7 @@ const whatBump = (commits) => {
       level = 0
     } else if (commit.type === 'feat') {
       features += 1
-      if (level === 2) {
+      if (level === 2 || level === undefined) {
         level = 1
       }
     } else if (['fix', 'patch', 'revert', 'refactor', 'docs', 'style'].includes(commit.type)) {
@@ -30,8 +30,8 @@ const whatBump = (commits) => {
   return {
     level: level,
     reason: breakings === 1
-      ? `There is ${breakings} BREAKING CHANGE, ${features} features and ${patches} pathes`
-      : `There are ${breakings} BREAKING CHANGES, ${features} features and ${patches} pathes`
+      ? `There is ${breakings} BREAKING CHANGE, ${features} features and ${patches} patches`
+      : `There are ${breakings} BREAKING CHANGES, ${features} features and ${patches} patches`
   }
 }
 
