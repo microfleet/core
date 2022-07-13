61cc970b9c636b4b3a076a3715dc9ec20d6f838d
AVVS <v@makeomatic.ca>
2022-07-13 10:40:03 -0700

    fix: increase test timeout for slow machines on CI

--- packages/plugin-router/jest.config.js
+++ packages/plugin-router/jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   ...globalConfig,
   rootDir: '../../',
   bail: 1,
+  testTimeout: 60000,
   collectCoverage: true,
   coverageDirectory: `packages/${dir}/coverage`,
   coverageReporters: [ "text-summary", "json", "lcov" ],
