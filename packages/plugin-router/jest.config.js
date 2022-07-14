dc417d64bb0c1416b73c6438a45f56b2638d8a24
AVVS <v@makeomatic.ca>
2022-07-14 12:03:15 -0700

    chore: specify order of tests

--- packages/plugin-router/jest.config.js
+++ packages/plugin-router/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ...globalConfig,
   rootDir: '../../',
   bail: 1,
-  testTimeout: 60000,
+  testTimeout: 30000,
   collectCoverage: true,
   coverageDirectory: `packages/${dir}/coverage`,
   coverageReporters: [ "text-summary", "json", "lcov" ],
