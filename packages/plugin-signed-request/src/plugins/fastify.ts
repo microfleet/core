24c525d9cecf68362e9b667ca03ab0d71fe6d522
pajgo <51755949+pajgo@users.noreply.github.com>
2022-10-24 12:37:16 +0600

    feat(plugin-signed-request): fastify plugin

---
+++ packages/plugin-signed-request/src/plugins/fastify.ts
@@ -0,0 +1,40 @@
+import type * as _ from '../types'
+
+import { FastifyInstance} from 'fastify'
+import fp from 'fastify-plugin'
+
+import { Config, CredentialsStore, SignedRequest } from '../signed-request'
+
+declare module 'fastify' {
+  interface FastifyRequest {
+    signature?: SignedRequest
+  }
+}
+
+export const FastifyRequestSignaturePlugin = (store: CredentialsStore, config: Config) => {
+  return fp(async function fastifyRequestSignaturePlugin(instance: FastifyInstance) {
+    instance.addHook('onRequest', async (req) => {
+      if (SignedRequest.isSignedRequest(req.raw.headers)) {
+        req.signature = new SignedRequest(config, store)
+      }
+    })
+  
+    instance.addHook('preParsing', async (req, _reply, payload) => {
+      if (req.signature) {
+        const { signature } = req
+        await signature.initialize(req.raw)
+        signature.verifyHeaders()
+
+        payload.on('data', (chunk: any) => { signature.appendPayload(chunk) })
+      }
+
+      return payload
+    })
+
+    instance.addHook('preValidation', async (req) => {
+      if (req.signature) {
+        req.signature.verifyPayload()
+      }
+    })
+  })
+}
