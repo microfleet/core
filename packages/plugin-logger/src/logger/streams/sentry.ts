d755df31c1937440ffb4e832d792a30bd1e93212
bludzhd <2344246+bludzhd@users.noreply.github.com>
2024-01-26 23:57:11 +0500

    fix(plugin-logger): pass error enumerable fields as event extras (#698)
    * fix(plugin-logger): pass error enumerable fields as event extras
    * fix(plugin-logger): test capture exception case

--- packages/plugin-logger/src/logger/streams/sentry.ts
+++ packages/plugin-logger/src/logger/streams/sentry.ts
@@ -10,7 +10,7 @@ export type SentryTransportConfig = {
   sentry: Sentry.NodeOptions
 }
 
-class ExtendedError extends Error {
+export class ExtendedError extends Error {
   constructor(message: string, public stack: string | undefined, public code: string | undefined, public signal: string | undefined) {
     super(message)
     this.name = "Error"
@@ -79,7 +79,7 @@ export async function sentryTransport({ externalConfiguration, sentry, minLevel 
 
       // extend scope with enumerable error properties if they exist, omit manually processed ones
       if (obj.err) {
-        for (const [key, prop] of Object.entries(obj)) {
+        for (const [key, prop] of Object.entries(obj.err)) {
           if (!kOmitKeys.includes(key)) {
             scope.setExtra(key, prop)
           }
