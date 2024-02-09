897927907424ea693ac7bbb59c4d58e7a9d6b728
Sasha S <sorokin.sh@gmail.com>
2024-02-09 21:03:57 +0300

    chore: test for single file

--- packages/plugin-redis-sentinel/__tests__/redis.spec.ts
+++ packages/plugin-redis-sentinel/__tests__/redis.spec.ts
@@ -50,6 +50,9 @@ describe('Redis suite', function testSuite() {
 
     const version = await service.redis.get('version')
     assert.strictEqual(version, '10')
+
+    const migration_01 = await service.redis.get('migration_01')
+    assert.strictEqual(migration_01, 'done')
   })
 
   it('able to close connection to redis', async () => {
