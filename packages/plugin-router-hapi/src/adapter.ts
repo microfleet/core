045b15dc867d30dae512fc35fa19fc32cda94a37
AVVS <v@makeomatic.ca>
2024-07-31 10:21:56 -0700

    feat: support error code in hapi respones as extra data

--- packages/plugin-router-hapi/src/adapter.ts
+++ packages/plugin-router-hapi/src/adapter.ts
@@ -3,9 +3,10 @@ import { noop } from 'lodash'
 import { Request } from '@hapi/hapi'
 import { boomify } from '@hapi/boom'
 
-import { Microfleet } from '@microfleet/core'
-import { ActionTransport, ServiceRequest } from '@microfleet/plugin-router'
+import type { Microfleet } from '@microfleet/core'
+import { ActionTransport, type ServiceRequest } from '@microfleet/plugin-router'
 import { HttpStatusError } from '@microfleet/validation'
+import assert from 'assert'
 
 declare module '@hapi/boom' {
   interface Payload {
@@ -13,12 +14,23 @@ declare module '@hapi/boom' {
   }
 }
 
+const kSupportedMethods = {
+  get: true,
+  post: true,
+  head: true,
+  delete: true,
+  put: true,
+  options: true,
+  patch: true,
+} as const
+
 export default function getHapiAdapter(actionName: string, service: Microfleet): (r: Request) => Promise<any> {
   const { router } = service
   // pre-wrap the function so that we do not need to actually do fromNode(next)
   const reformatError = (error: any) => {
     let statusCode
     let errorMessage
+    let errorCode
 
     const { errors } = error
 
@@ -49,10 +61,11 @@ export default function getHapiAdapter(actionName: string, service: Microfleet):
       } else {
         const [nestedError] = errors
         errorMessage = nestedError.text || nestedError.message || undefined
+        errorCode = nestedError.code || undefined
       }
     }
 
-    const replyError = boomify(error, { statusCode, message: errorMessage })
+    const replyError = boomify(error, { statusCode, message: errorMessage, data: { errorCode } })
 
     if (error.name) {
       replyError.output.payload.name = error.name
@@ -64,6 +77,9 @@ export default function getHapiAdapter(actionName: string, service: Microfleet):
   return async function handler(request: Request) {
     const { headers } = request
 
+    // @ts-expect-error narrow down
+    assert(kSupportedMethods[request.method] === true)
+
     const serviceRequest: ServiceRequest = {
       // defaults for consistent object map
       // opentracing
@@ -74,7 +90,7 @@ export default function getHapiAdapter(actionName: string, service: Microfleet):
       action: noop as any,
       locals: Object.create(null),
       log: console as any,
-      method: request.method,
+      method: request.method as keyof typeof kSupportedMethods,
       params: request.payload,
       query: request.query,
       route: actionName,
